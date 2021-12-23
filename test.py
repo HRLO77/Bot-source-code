ba2c074b2f1e9ab0b29ebfe9ad77b9a659c9c635
HRLO77 <95102676+HRLO77@users.noreply.github.com>
2021-12-23 13:25:54 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,7 @@
 import Functions
 
 list = []
-set = {'cunt', 'mur', 'pussy', 'sex', 'pis', 'frick', 'nigga', 'nazi', 'sht', 'sh*t', 'rape', 'crap',
-                  'n*gger', 'xxx', 'neo', 'rupe', 'naz*', 'dam', 'asre', 'dummy', 'th*t', 'fort', 'thot', 'fk', 'prod',
-                  'v*gina', 'pok', 'gay', 'lmb', 'peg', 'hac', 'geno', 'dayum', 'hit', 'job', 'n!gger', 'p*rn', 'flip',
-                  'p*ss', 'idoit', 'damn', 'p*ssy', 'dump', 'nigger', 'p!s', 'fck', 'sta', 'crud', 'segs', 'shit',
-                  'c!ck', 's*ck', 'bastard', 'kil', 'fuc', 'porn', 'cu', 't!t',
-                  'tit', 'fack', 'sh!t', 'butt', 'danm', 'dip', 's*x', 'd!ck', 'gai', 'idit', 'cock', 'fuke', 'puss',
-                  'trash', 'slap', 'seggs', 'anal', 'sx', 'fac', 'idiot', 'c*nt', 'stup', 'fr*ck', 'n!gga', 'rob',
-                  'mutil', 'dumb', 't*t', 'stfu', 'clusterfuc', 'blo', 'hitler', 'lana', 'rod', 'fuk', 'nite', 'suck',
-                  'fuck', 'btch', 'lma', 'pus', 'p!ss', 'idg', 'cr*p', 'n*zi', 'robl', 'piss', 'shut', 'ass', 'bitch',
-                  'fu', 'prn', 'arse', 'stick', 'fruck', 'suc', 'bang', 'dum', 'vagina', 'come', 'dick'}
+set = {'List of swear words go hear'}
 for i in set:
     for index, value in enumerate(str(i).replace(' ', '')):
         if ' ' in i:
